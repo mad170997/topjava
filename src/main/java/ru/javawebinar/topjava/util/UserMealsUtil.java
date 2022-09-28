ab212f855e4a4e06e3ca8779c1c80d6297a3c20b
Anastasiia <mad170997@gmail.com>
2022-09-28 15:37:48 +0300

    Implemented methods UserMealsUtil.filteredByStreams, UserMealsUtil.filteredByCycles

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -33,8 +33,8 @@ public class UserMealsUtil {
         Map<LocalDate, Integer> dateAndCaloriesPerDay = new HashMap<>();
         return meals.stream()
                 .peek(x->dateAndCaloriesPerDay.merge(x.getDateTime().toLocalDate(),x.getCalories(),Integer::sum))
-                .sorted(Comparator.comparing(UserMeal::getDateTime))
                 .filter(x->TimeUtil.isBetweenHalfOpen(x.getDateTime().toLocalTime(),startTime,endTime))
+                .sorted(Comparator.comparing(UserMeal::getDateTime))
                 .map(x -> dateAndCaloriesPerDay.get(x.getDateTime().toLocalDate())>caloriesPerDay ? new UserMealWithExcess(x.getDateTime(), x.getDescription(), x.getCalories(), true) :
                         new UserMealWithExcess(x.getDateTime(), x.getDescription(), x.getCalories(), false))
                 .collect(Collectors.toList());
